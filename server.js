9620741aae33599574f5cb98ab49e39cc1513373
Wander Junior <wander.junior@infracommerce.com.br>
2018-10-03 13:03:33 -0300

    Teste deploy heroku 4

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
 
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port);
